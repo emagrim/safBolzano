ffaa51fa3ceab910b1a58f41ee202d0a20b62e66
Alex De Carli <alexdecarli07@outlook.it>
2024-02-18 00:08:14 +0100

    time to save
    00:00 finally slider works 100%
    (how much time have I lost? a lifetime, but i loved the result)

--- public/scripts/introducer.js
+++ public/scripts/introducer.js
@@ -3,35 +3,97 @@ document.addEventListener('DOMContentLoaded', function () {
     const slider = document.querySelector('.containsSlides');
     const sliderWidth = slider.clientWidth;
     let currentSlide = 0;
+    let isScrolling = false;
 
-    function scrollSlider(direction) {
-        const scrollAmount = direction === 'next' ? slider.clientWidth : -slider.clientWidth;
-        slider.scrollTo({
-            left: slider.scrollLeft + scrollAmount,
+    const element1 = document.querySelector('.sliderButton.left');
+    const element2 = document.querySelector('.sliderButton.right');
+    const thereIsInCode = element1 && element2;
+
+    /*slides.forEach(function (slide) {
+        var img = container.querySelector('img');
+      
+        if (img.naturalHeight > img.naturalWidth) {
+          console.log('vertical.');
+        } else if(img.naturalHeight = img.naturalWidth) {
+          console.log('square.');
+        } else {
+            console.log('horizzontal')
+        }
+      });*/
+
+    function scrollSlider(direction, callback) {
+        if (thereIsInCode) {
+            document.querySelector('.sliderButton.right').removeEventListener('click', alwaysNextSlide);
+            const scrollAmount = direction === 'next' ? slider.clientWidth : -slider.clientWidth;
+            slider.scrollTo({
+                left: slider.scrollLeft + scrollAmount,
+                behavior: 'smooth'
+            });
+
+            setTimeout(function () {
+                document.querySelector('.sliderButton.right').addEventListener('click', alwaysNextSlide);
+            }, 1000);
+        } else {
+            const scrollAmount = direction === 'next' ? slider.clientWidth : -slider.clientWidth;
+            slider.scrollTo({
+                left: slider.scrollLeft + scrollAmount,
+                behavior: 'smooth'
+            });
+        }
+    }
+
+    function toStart() {
+        const toStart = slider.clientWidth * slides.length;
+        slider.scrollBy({
+            left: -toStart,
             behavior: 'smooth'
         });
     }
+
+
 
     function prevSlide() {
         currentSlide = (currentSlide - 1 + slides.length) % slides.length;
         scrollSlider('prev');
     }
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slides.length;
-        scrollSlider('next');
 
-        if (currentSlide === slides.length - 1) {
-            setTimeout(() => {
-                currentSlide = 0;
-                scrollSlider('prev');
-            }, 6000);
-        } else {
-            setTimeout(nextSlide, 6000);
+    let isTimeoutFinished = true;
+
+    function nextSlide() {
+        if (isTimeoutFinished) {
+            isTimeoutFinished = false;
+
+            setTimeout(function () {
+                isTimeoutFinished = true;
+            }, 5000);
+
+            currentSlide = (currentSlide + 1) % slides.length;
+            scrollSlider('next');
+
+            if (currentSlide === slides.length - 1) {
+                setTimeout(() => {
+                    currentSlide = 0;
+                    toStart();
+                    setTimeout(nextSlide, 6000);
+                }, 6000);
+            } else {
+                setTimeout(nextSlide, 6000);
+            }
         }
     }
 
+    function alwaysNextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        scrollSlider('next');
+
+    }
+
+
     setTimeout(nextSlide, 6000);
 
-    document.querySelector('.sliderButton.left').addEventListener('click', prevSlide);
-    document.querySelector('.sliderButton.right').addEventListener('click', nextSlide);
+
+    if (thereIsInCode) {
+        document.querySelector('.sliderButton.left').addEventListener('click', prevSlide);
+        document.querySelector('.sliderButton.right').addEventListener('click', alwaysNextSlide);
+    }
 });
